762aa18522af4d8188de99ee0b11535b399b7283
RICKCOYL <rickoburu@gmail.com>
2020-12-27 11:52:07 +0300

    add views in both post and user model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    def new; end
+
+    def create
+        @user = User.find_by(username: params[:session][:username])
+        if @user
+          session[:user_id] = @user.id
+          flash[:notice] = "#{@user.username} succesfully logged in"
+          redirect_to posts_path
+        else
+          flash.now.alert = 'Login failed'
+          render :new
+        end
+    end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have successfully logged out.'
+    redirect_to new_user_path
+  end
+
+  
+end
