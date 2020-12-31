9cbceac5f42ba81fefacef410e36b117bcf01db5
RICKCOYL <rickoburu@gmail.com>
2020-12-31 12:05:23 +0300

    add to post time it was created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
-  resources :posts
+  resources :posts, exept: [:edit, :destroy]
   resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'sessions/new'
