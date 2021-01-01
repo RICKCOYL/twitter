274e784a40624cb64161132b57edb8d3c9a7266f
RICKCOYL <rickoburu@gmail.com>
2021-01-01 22:39:40 +0300

    add more testing to post controller

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+  describe "GET index" do
+    it 'returns a successful response' do
+      get '/posts'
+      expect(response).to be_truthy
+    end
+ 
+    it "assigns @users" do
+      post = Post.create(body: 'Test user')
+      get '/posts'
+      expect(assigns(:posts)).== ([post])
+    end
 
+  end
 end
