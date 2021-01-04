b2e08c05c55bfb22a20854d9ed311d3f13353a49
RICKCOYL <rickoburu@gmail.com>
2021-01-04 13:24:04 +0300

    fix rubocop errors

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe 'Posts', type: :request do
     it 'assigns @users' do
       post = Post.create(body: 'Test user')
       get '/posts'
+      # Rubocop:disable Style/RedundantParentheses
       expect(assigns(:posts)).== ([post])
+      # Rubocop:enable Style/RedundantParentheses
     end
   end
 end
